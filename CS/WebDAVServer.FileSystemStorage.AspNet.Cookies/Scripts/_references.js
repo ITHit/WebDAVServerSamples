6bc916b4798c619cbab8c628a0012b85b8478237
IT Hit <build@ithit.com>
2019-05-22 03:18:59 +0300

    v8.0.5014-Beta

--- CS/WebDAVServer.FileSystemStorage.AspNet.Cookies/Scripts/_references.js
+++
@@ -1,7 +0,0 @@
-/// <autosync enabled="true" />
-/// <reference path="bootstrap.min.js" />
-/// <reference path="jquery.validate.js" />
-/// <reference path="jquery.validate.unobtrusive.js" />
-/// <reference path="jquery-1.10.2.js" />
-/// <reference path="modernizr-2.6.2.js" />
-/// <reference path="respond.js" />
